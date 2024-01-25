e84cd2094a64430328f6739c364f8a4a27459b2d
Elizabeth <elizabethhsy12@gmail.com>
2024-01-25 16:50:38 +0000

    interactive demo of sphere changing colour

--- visualisations/src/main.ts
+++ visualisations/src/main.ts
@@ -1,3 +1,4 @@
+import { randomInt } from "crypto"
 import * as THREE from "three"
 
 const width = window.innerWidth
@@ -13,18 +14,43 @@ const mainCamera = new THREE.PerspectiveCamera(60, width / height, 0.1, 100)
 
 const scene = new THREE.Scene()
 
-const geometry = new THREE.BoxGeometry()
+const geometry = new THREE.SphereGeometry()
 const material = new THREE.MeshPhongMaterial({ color: 0xFFAD00 })
 
-const cube = new THREE.Mesh(geometry, material)
-cube.position.z = -5
-cube.position.y = -1
+const sphere = new THREE.Mesh(geometry, material)
+sphere.position.z = -5
+sphere.position.y = 0
 
-scene.add(cube)
+scene.add(sphere)
 
 const light = new THREE.DirectionalLight(0xFFFFFF, 1)
 light.position.set(0, 4, 1)
 
 scene.add(light)
 
+const light2 = new THREE.DirectionalLight(0xFFFFFF, 1)
+light2.position.set(1, -3, 3)
+
+scene.add(light2)
+
 renderer.render(scene, mainCamera)
+
+let array = [0xFF0000, 0x00FF00, 0x0000FF]
+let i = 0;
+
+async function playAnimation() {
+	while (true) {
+		material.color.setHex(array[i]);
+		i = (i+1)%array.length;
+		renderer.render(scene, mainCamera)
+		await new Promise(f => setTimeout(f, 1000));
+	}
+}
+
+document.getElementById("animation")?.addEventListener("click", playAnimation);
+
+type CO2Emission = {
+	timestamp: number;
+	emission_per_layer: number[];
+}
+
