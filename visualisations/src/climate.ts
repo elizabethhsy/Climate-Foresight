c83ae0b047ffef9d344735e4a2147390d55d0e4f
Elizabeth <elizabethhsy12@gmail.com>
2024-02-03 13:13:53 +0000

    added folder for csv data

--- visualisations/src/climate.ts
+++ visualisations/src/climate.ts
@@ -1,6 +1,21 @@
 import * as THREE from "three"
-import csv from "../../../data/climate/pos_generative.csv"
+import csv from "../../data/climate/pos_generative.csv"
 import Globe from 'globe.gl';
 
-Globe().globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
-(document.getElementById('globeViz'))
+const N = 300;
+const gData = [...Array(N).keys()].map(() => ({
+    lat: (Math.random() - 0.5) * 160,
+    lng: (Math.random() - 0.5) * 360,
+    weight: Math.random()
+}));
+
+const world = Globe()
+    .globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg')
+    .heatmapsData([gData])
+    .heatmapPointLat('lat')
+    .heatmapPointLng('lng')
+    .heatmapPointWeight('weight')
+    .heatmapTopAltitude(0.7)
+    .heatmapsTransitionDuration(3000)
+    .enablePointerInteraction(false)
+    (document.getElementById('globeViz'));
