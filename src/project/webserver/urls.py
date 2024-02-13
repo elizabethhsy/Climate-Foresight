4801a417dd69a7cbbd35efea99cf9676be5e794e
Oliver Gaskell <og305@cam.ac.uk>
2024-02-13 13:47:37 +0000

    Add functional log in and log out

--- src/project/webserver/urls.py
+++ src/project/webserver/urls.py
@@ -1,9 +1,24 @@
 """Main Webserver URL definitions."""
 
-from django.urls import path
+from django.urls import include, path
 from . import views
+
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('test/', views.TestPage.as_view()),
+
+    # path("account/", include("django.contrib.auth.urls")),
+    path(
+        "account/login/",
+        auth_views.LoginView.as_view(next_page="/"),
+        name="login"
+    ),
+    path(
+        "account/logout/",
+        auth_views.LogoutView.as_view(next_page="/"),
+        name="logout",
+    ),
+
     path('', views.HomePage.as_view())
 ]
