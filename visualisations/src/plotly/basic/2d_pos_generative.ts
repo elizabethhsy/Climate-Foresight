d8454b1ac0d0e31d4badae94ca8cf011eecbac40
Kai Vennemann <56052633+kaivennemann@users.noreply.github.com>
2024-02-18 22:53:01 +0000

    subplots in plotly

--- visualisations/src/plotly/basic/2d_pos_generative.ts
+++ visualisations/src/plotly/basic/2d_pos_generative.ts
@@ -3,83 +3,43 @@ import data from '../../../../data/climate/ssp585/clean/pos_generative.json' ass
 
 // Wrangle data
 var graphData = []; // data to be graphed
-let numPoints = data["year"].length;
-let columns = Object.keys(data).filter(x => x != 'year'); // test
+let year = data["year"];
+let numPoints = year.length;
+let columns = Object.keys(data).filter(x => x != 'year');
+let nrows = 6;
+let ncols = 4;
 
-// for (let row = 1; row <= 3; row += 1) {
-//     for (let col = 1; col <= 4; col += 1) {
-//         let year = data["year"].slice(start, end);
-//         let atmosphericTemp = data["atmospheric_temp"].slice(start, end);
+for (let row = 1; row <= nrows; row += 1) {
+    for (let col = 1; col <= ncols; col += 1) {
+        let column = columns[(row - 1) * ncols + (col - 1)];  // index into the columns array
+        let y = data[column];
+        let i = (row - 1) * ncols + col;
         
-//         graphData.push({
-//             name: 'run ' + r,
-//             type: 'surface',
-//             x: year.map(v => [v,v]),
-//             y: Array.from({length: numPoints}, () => [r - 0.375, r + 0.375]),
-//             z: atmosphericTemp.map(v => [v,v]),
-//             colorscale: 'Picnic',
-//             showscale: false,
-//             lighting: {specular: 0.3}
-//         })
-
-//     }
-// }
+        graphData.push({
+            name: column,
+            type: 'scatter',
+            x: year,
+            y: y,
+            xaxis: 'x' + i,
+            yaxis: 'y' + i
+        })
+    }
+}
 
 // Prepare graph inputs
 var graphDiv = document.getElementById('graph');
 
 var layout = {
-    title: {
-      text: 'Atmospheric Temperature (1750-2100)',
-      font: {
-        family: 'Courier New, monospace',
-        size: 28
-      },
-      yref: 'paper',
-      y: 0.95
-    },
+    grid: {rows: nrows, columns: ncols, pattern: 'independent'},
     autosize: true,
-    scene: { // specifies 3D scene
-        xaxis: {
-            title: 'Year',
-            showgrid: true,
-            zeroline: true,
-            autorange: 'reversed'  // reverse the year axis to show earlier years closer to viewer
-        },
-        yaxis: {
-            title: 'Run',
-            showgrid: false,
-            zeroline: true
-        },
-        zaxis: {
-            title: 'Atmospheric Temperature',
-            showgrid: true,
-            zeroline: true
-        }
-    },
-    showlegend: true,
-    legend: {
-        title: {
-            text: 'Legend',
-        },
-        x: 1.05, // position legend outside the plot area to the right
-        xanchor: 'left',
-        y: 0.5,
-        yanchor: 'middle'
-    },
-    margin: { // make sure labels are visible
-        l: 0,
-        r: 0,
-        b: 0,
-        t: 0
-    }
+    height: 2000
 };
 
 var config = {
     responsive: true,       // graph size adjusts to window
     displayModeBar: true,   // helper icons at top right
     displaylogo: false,     // Plotly logo
-    modeBarButtonsToRemove: ['zoom3d', 'pan3d', 'orbitRotation', 'tableRotation', 'resetCameraLastSave3d']
+    modeBarButtonsToRemove: []
 }
 
 // Create ribbon graph
