44a7822de6014ca3d818a6844a3c22b6ac996ada
Kai Vennemann <56052633+kaivennemann@users.noreply.github.com>
2024-02-19 14:28:19 +0000

    added subplot titles

--- visualisations/src/plotly/basic/2d_pos_generative.ts
+++ visualisations/src/plotly/basic/2d_pos_generative.ts
@@ -3,8 +3,8 @@ import data from '../../../../data/climate/ssp585/clean/pos_generative.json' ass
 
 // Wrangle data
 var graphData = []; // data to be graphed
+var annotations = [];
 let year = data["year"];
-let numPoints = year.length;
 let columns = Object.keys(data).filter(x => x != 'year');
 let nrows = 6;
 let ncols = 4;
@@ -21,7 +21,21 @@ for (let row = 1; row <= nrows; row += 1) {
             x: year,
             y: y,
             xaxis: 'x' + i,
-            yaxis: 'y' + i
+            yaxis: 'y' + i,
+            hovertemplate:
+                `${column.replaceAll('_', ' ')}: %{y:.1f}<br>`
+                + `Year: %{x}`
+                + `<extra></extra>`,
+        })
+
+        annotations.push({
+            text: column,
+            xref: 'paper',
+            yref: 'paper',
+            x: (col - (0.75 * (4 - col)/3 + 0.25 * (col - 1)/3)) / ncols,  // weighted average to have centered titles
+            y: 1.0 - (row - (1.0 * (6-row)/5 + 0.8 * (row-1)/5)) / nrows,
+            yanchor: 'bottom',
+            showarrow: false
         })
     }
 }
@@ -30,9 +44,12 @@ for (let row = 1; row <= nrows; row += 1) {
 var graphDiv = document.getElementById('graph');
 
 var layout = {
+    title: 'Posterior Generative Data',
     grid: {rows: nrows, columns: ncols, pattern: 'independent'},
+    annotations: annotations,
     autosize: true,
-    height: 2000
+    height: 2000,
+    showlegend: false
 };
 
 var config = {
