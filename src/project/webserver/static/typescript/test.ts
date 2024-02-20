165d76c6e1cb446922f65fe3cd809cb67bc34cdb
Jacob Liu <jacobliu001@qq.com>
2024-02-20 07:48:35 +0000

    Revert test.ts

--- src/project/webserver/static/typescript/test.ts
+++ src/project/webserver/static/typescript/test.ts
@@ -1,21 +1,2 @@
 let helloworld:string = "Hello World!\nTypeScript is working :)";
 alert(helloworld);
-
-let url:string = "/api/test/";
-fetch(url).then(response => response.json()).then(json => alert(json["msg"]));
-
-let payload: string = "Hello from the frontend!";
-fetch(url, {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    body: payload
-}).then(response => response.json()).then(
-    json => {
-        alert(json["msg"]);
-        if (payload != json["echo"]) {
-            alert("Error: Payloads do not match");
-        }
-    }
-);
