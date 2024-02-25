788ac9533ef364ef67f3fa4c3fb0417b2bafee86
Oliver Gaskell <og305@cam.ac.uk>
2024-02-25 11:49:35 +0000

    Add radiobutton test to climate.html

--- src/project/webserver/static/typescript/visualisations/climate.ts
+++ src/project/webserver/static/typescript/visualisations/climate.ts
@@ -21,18 +21,27 @@ async function setup_barchart(): void {
     const url = '/api/climate?scenario=ssp126&file=pos_generative';
     const url_data = await fetch(url);
     const data = await url_data.json();
-    
+
     const configparent = document.getElementById("config-0-parent");
 
     const config = new Config(0);
     config.add_value("checkbox0", "Checkbox 0", "checkbox");
     config.add_value("checkbox1", "Checkbox 1", "checkbox");
+    config.add_value("radio0", "Radio 0", "radiobutton", ["Radio Opt 1", "Radio Opt 2", "Radio Opt 3"]);
 
     config.instantiate(configparent);
-    
+
     // Create bar chart
     const graphElement = document.getElementById('chart');
     const plotlyGraph = new BarChart(graphElement, config);
     plotlyGraph.prepareData(data);
     plotlyGraph.create();
+
+    document.getElementById("config-0-test").onclick = function () {
+        alert(
+            "Checkbox 0: " + config.values["checkbox0"] + "\n" +
+            "Checkbox 1: " + config.values["checkbox1"] + "\n" +
+            "Radio 0: " + config.values["radio0"]
+        );
+    }
 }
