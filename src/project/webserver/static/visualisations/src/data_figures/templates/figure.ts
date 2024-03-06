edca9e754949df105eae38f7d92e488c251ef82e
Kai Vennemann <56052633+kaivennemann@users.noreply.github.com>
2024-03-06 15:08:29 +0000

    fixed hover plots and wrote comments

--- src/project/webserver/static/visualisations/src/data_figures/templates/figure.ts
+++ src/project/webserver/static/visualisations/src/data_figures/templates/figure.ts
@@ -11,6 +11,21 @@ abstract class Figure {
         }
     }
 
+    protected getUnit(metric: string) {
+        switch(metric) {
+            case "forcing":
+                return "W/m2";
+            case "concentration":
+                return "ppm";
+            case "emissions":
+                return "million tonnes";
+            case "airborne_emissions":
+                return "million tonnes";
+            default:
+              return "K"
+        }
+    }
+
     // Function to call on load but before rendering
     abstract init(): void;
     
